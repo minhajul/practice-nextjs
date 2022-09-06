2e0d42450fa2cdffdef8a9dc6aa5e85e84bb0333
minhajul <kmsohelinfo@gmail.com>
2022-09-06 11:44:34 +0600

    Added loading statre

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import {getAllProducts, getProductByHandle} from "../../libs/shopify";
 import {formatter} from '../../libs/helper'
-
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -12,8 +11,13 @@ import {FreeMode, Navigation, Thumbs} from "swiper";
 
 export default function ProductPage({product}) {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     const price = product.priceRange.minVariantPrice.amount
+
+    const checkout = () => {
+        setIsLoading(true)
+    }
 
     return (
         <div className="bg-white">
@@ -50,7 +54,7 @@ export default function ProductPage({product}) {
                             loop={true}
                             spaceBetween={10}
                             navigation={true}
-                            thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+                            thumbs={{swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null}}
                             modules={[FreeMode, Navigation, Thumbs]}
                             className="mySwiper2"
                         >
@@ -85,9 +89,18 @@ export default function ProductPage({product}) {
 
                         <form className="mt-6">
                             <div className="mt-10 flex sm:flex-col1">
-                                <button type="submit"
-                                        className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full">
-                                    Add to bag
+                                <button
+                                    onClick={checkout}
+                                    type="submit"
+                                    className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full">
+
+                                    {isLoading && (
+                                        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    )}
+                                    Buy
                                 </button>
                             </div>
                         </form>
